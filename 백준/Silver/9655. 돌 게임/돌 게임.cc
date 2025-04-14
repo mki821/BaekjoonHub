07e3d2f229b5bd6716f8799db82b3e06f82cdd77
mki821 <123716001+mki821@users.noreply.github.com>
2025-04-14 19:54:23 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -5,14 +5,9 @@ int main() {
 	cin.tie(0)->sync_with_stdio(false);
 
 	int n;
-	int dp[1001];
 
 	cin >> n;
 
-	int key = n % 4;
-
-	if (key == 0) cout << "CY";
-	else if (key == 1) cout << "SK";
-	else if (key == 2) cout << "CY";
+	if (n % 2 == 0) cout << "CY";
 	else cout << "SK";
 }
